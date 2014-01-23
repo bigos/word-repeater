0b6fdf246e0d14cef93483a49beae488dc773c48
Jacek Podkanski <ruby.object@googlemail.com>
2014-01-23 23:31:55 +0000

    changed licence data

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject word-repeater "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU GENERAL PUBLIC LICENSE Version 2"
+            :url "http://www.gnu.org/licenses/gpl-2.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot word-repeater.core
   :target-path "target/%s"
