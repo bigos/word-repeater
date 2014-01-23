9ae2e24e3377b689fa31114171abbb7fa4b4b4c1
Jacek Podkanski <ruby.object@googlemail.com>
2014-01-23 23:23:31 +0000

    created new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to word-repeater
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/word_repeater/core_test.clj
@@ -0,0 +1,7 @@
+(ns word-repeater.core-test
+  (:require [clojure.test :refer :all]
+            [word-repeater.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
