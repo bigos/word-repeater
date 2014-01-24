dacc7a1abd1ca6566648b5102f4ef744e9e8e71f
Jacek Podkanski <ruby.object@googlemail.com>
2014-01-24 00:27:12 +0000

    copied some code from test project

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "GNU GENERAL PUBLIC LICENSE Version 2"
             :url "http://www.gnu.org/licenses/gpl-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-yaml "0.4.0"]
+                 [seesaw "1.4.2"]
+                 [com.github.insubstantial/substance "7.1"]
+                 [org.clojars.technomancy/jlayer "1.0"]]
   :main ^:skip-aot word-repeater.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/word_repeater/core.clj
+++ src/word_repeater/core.clj
@@ -1,5 +1,21 @@
 (ns word-repeater.core
+  (:use [seesaw.core])
+  (:import  org.pushingpixels.substance.api.SubstanceLookAndFeel)
+  (:require [clojure.string :as str]
+            [clj-yaml.core :as yaml]
+            [clojure.java.io :as io])
   (:gen-class))
+
+(defn play-file [filename & opts]
+  (let [fis (java.io.FileInputStream. filename)
+        bis (java.io.BufferedInputStream. fis)
+        player (javazoom.jl.player.Player. bis)]
+    (if-let [synchronously (first opts)]
+      (doto player
+        (.play)
+        (.close))
+      (.start (Thread. #(doto player (.play) (.close)))))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
